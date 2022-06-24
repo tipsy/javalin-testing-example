8abf7562dce92f400eb8a9961e9c5321acf04bee
Mikolaj Matuszewski <mikolaj.matuszewski@digitalnewagency.com>
2022-06-24 10:13:27 +0200

    Replaced TestUtil with JavalinTest, upgraded javalin-bundle dependency
    Signed-off-by: Mikolaj Matuszewski <mikolaj.matuszewski@digitalnewagency.com>

--- src/test/java/io/javalin/example/java/FunctionalTest.java
+++ src/test/java/io/javalin/example/java/FunctionalTest.java
@@ -2,7 +2,7 @@ package io.javalin.example.java;
 
 import io.javalin.Javalin;
 import io.javalin.plugin.json.JavalinJackson;
-import io.javalin.testtools.TestUtil;
+import io.javalin.testtools.JavalinTest;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,7 +14,7 @@ public class FunctionalTest {
 
     @Test
     public void GET_to_fetch_users_returns_list_of_users() {
-        TestUtil.test(app, (server, client) -> {
+        JavalinTest.test(app, (server, client) -> {
             assertThat(client.get("/users").code()).isEqualTo(200);
             assertThat(client.get("/users").body().string()).isEqualTo(usersJson);
         });
